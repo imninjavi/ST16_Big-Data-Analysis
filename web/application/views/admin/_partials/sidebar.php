73b8eb94772fc1ca0172121bf61c47837c7e9636
ninjavi98 <ninja.vi98@gmail.com>
2020-01-13 15:10:12 +0700

    Partial template made

---
+++ web/application/views/admin/_partials/sidebar.php
@@ -0,0 +1,19 @@
+<!-- Sidebar -->
+<ul class="sidebar navbar-nav">
+    <li class="nav-item <?php echo $this->uri->segment(2) == '' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin') ?>">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Overview</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Charts</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Tables</span></a>
+    </li>
+</ul>
